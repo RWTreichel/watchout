7bf4b79f135f56a37494262e2b2e5954b2e56537
Richard Treichel <rwtreichel@gmail.com>
2015-05-22 22:54:20 -0400

    Added collision detected

--- watchout.js
+++ watchout.js
@@ -3,25 +3,18 @@ var width = 500;
 var height = 400;
 var numEnemies = 10;
 
-var gameBoard = d3.select('body').append('svg:svg')
-    .attr('width', width)
-    .attr('height', height)
-    .style('background-color', "black");  
+var gameBoard = d3.select('body')
+                  .append('svg:svg')
+                  .attr('width', width)
+                  .attr('height', height)
+                  .style('background-color', "black");  
 
-var createPlayer = function(){
-  return [{
-    id: 0,
-    x: width/2,
-    y: height/2,
-  }];
-}
-
-var Player = function(player_data){
+var Player = function(){
   var drag = d3.behavior.drag()
                .on('drag', function(){
                 player.attr('cx', d3.event.x)
                       .attr('cy', d3.event.y);
-               })
+               });
 
   var player = gameBoard.selectAll('.player')
                         .data([{x: (width/2), y: (height/2), r: 10}])
@@ -33,11 +26,10 @@ var Player = function(player_data){
                         .attr('r', function(d) {return d.r})
                         .call(drag)
                         .style('fill', 'blue');
-
   return Player;
 };
 
-Player(createPlayer());
+Player();
 
 var createEnemies = function(){
   return _.range(0, numEnemies).map(function(i) {
@@ -49,6 +41,49 @@ var createEnemies = function(){
   });
 };
 
+var checkCollision = function(enemy, collidedCb) {
+  var player = d3.select('.player');
+  var radiusSum = parseFloat(enemy.attr('r')) + parseFloat(player.attr('r'));
+  var xDiff = parseFloat(enemy.attr('cx')) - parseFloat(player.attr('cx'));
+  var yDiff = parseFloat(enemy.attr('cy')) - parseFloat(player.attr('cy'));
+  
+  var distance = Math.sqrt( Math.pow(xDiff, 2) + Math.pow(yDiff, 2) );
+
+  if (distance < radiusSum) {
+    collidedCb(player, enemy);
+  }
+};
+
+var tweenCollision = function(endData) {
+  var enemy = d3.select(this);
+
+  var startPos = {
+    x: parseFloat(enemy.attr('cx')),
+    y: parseFloat(enemy.attr('cy'))
+  };
+
+  var endPos = {
+    x: endData.x,
+    y: endData.y
+  };
+
+
+  return function(t) {
+    checkCollision(enemy, function() {
+      console.log('collision detected');
+      //TODO: this
+    });
+
+    var enemyNextPos = {
+      x: startPos.x + (endPos.x - startPos.x) * t,
+      y: startPos.y + (endPos.y - startPos.y) * t
+    };
+
+    enemy.attr('cx', enemyNextPos.x)
+         .attr('cy', enemyNextPos.y);
+  };
+};
+
 var render = function(enemy_data){
   var enemies = gameBoard.selectAll('circle.enemy')
     .data(enemy_data, function(d){
@@ -56,16 +91,16 @@ var render = function(enemy_data){
     });
 
   enemies.enter()
-    .append('svg:circle')
-    .style('fill', 'red')
-    .attr('class', 'enemy')
-    .attr('cx', function(enemy){
-      return enemy.x;
-    })
-    .attr('cy', function(enemy){
-      return enemy.y;
-    })
-    .attr('r', 10);
+         .append('svg:circle')
+         .style('fill', "red")
+         .attr('class', 'enemy')
+         .attr('cx', function(enemy){
+           return enemy.x;
+         })
+         .attr('cy', function(enemy){
+           return enemy.y;
+         })
+         .attr('r', 10);
 
   enemies.exit()
     .remove();
@@ -76,11 +111,12 @@ var render = function(enemy_data){
       .each(function(){
         d3.select(this).transition()
           .attr('cx', Math.random() * width)
-          .attr('cy', Math.random() * height);
+          .attr('cy', Math.random() * height)
+          .tween('custom', tweenCollision);
       });
 
   return;
-}
+};
 
 
 render(createEnemies);
